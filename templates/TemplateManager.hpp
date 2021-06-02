b21e79e59903b8b2646b2bf5283de1d8ad3adabd
HJfod <60038575+HJfod@users.noreply.github.com>
2021-06-02 20:21:22 +0300

    que

--- templates/TemplateManager.hpp
+++ templates/TemplateManager.hpp
@@ -2,4 +2,17 @@
 
 #include <GDMake.h>
 
+class Template;
 
+class TemplateManager : public gd::GManager {
+    std::vector<Template*> m_vTemplates;
+
+    virtual bool init() override;
+
+    virtual void encodeDataTo(DS_Dictionary* data);
+    virtual void dataLoaded(DS_Dictionary* data);
+    virtual void firstLoad();
+
+    public:
+        static TemplateManager* sharedState();
+};
