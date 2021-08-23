c1cdf84e990c5eed87bfa2484e1ac96809350dfe
hjfod <dreadrollmusic@gmail.com>
2021-08-23 10:37:15 +0300

    upd commit.bat

--- commit.bat
+++ commit.bat
@@ -7,7 +7,10 @@ git add --all
 git commit -a
 git push origin master
 
+rem one of these will fail and the other'll work for me lol
+
 cd K:\gdmake\bin\Debug\net5.0\win10-x64\submodules
+cd C:\Users\HJfod\Documents\github\GDMake\bin\Debug\net5.0\win10-x64\submodules
 
 echo.
 echo ==== gd.h ====
