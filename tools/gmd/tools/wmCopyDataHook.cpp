84c336b00acfe0fe29c5ac33443add763de43245
HJfod <60038575+HJfod@users.noreply.github.com>
2021-10-12 22:49:22 +0300

    drag and drog files, including .zips cuzynot

--- tools/gmd/tools/wmCopyDataHook.cpp
+++ tools/gmd/tools/wmCopyDataHook.cpp
@@ -1,6 +1,13 @@
 #include "../../../BetterEdit.hpp"
 #include "passGmdFile.hpp"
 #include "../layers/goToHook.hpp"
+#include <unzipper.h>
+
+using namespace zipper;
+
+namespace gmd {
+    bool isLevelFileName(std::string const& fname);
+}
 
 GDMAKE_HOOK("libcocos2d.dll" + 0x113090)
 LRESULT __stdcall CCEGLView_WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -25,6 +32,42 @@ LRESULT __stdcall CCEGLView_WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARA
             SetFocus(getGDHWND());
             SetActiveWindow(getGDHWND());
         }
+
+        return TRUE;
+    }
+
+    if (msg == WM_DROPFILES) {
+        std::vector<std::string> files;
+        auto hDropInfo = as<HDROP>(wParam);
+        char sItem[MAX_PATH];
+        for(int i = 0; DragQueryFileA(hDropInfo, i, as<LPSTR>(sItem), sizeof(sItem)); i++) {
+            if (GetFileAttributesA(sItem) & FILE_ATTRIBUTE_DIRECTORY) {
+                for (auto const& file : std::filesystem::directory_iterator(sItem)) {
+                    if (gmd::isLevelFileName(file.path().string())) {
+                        files.push_back(file.path().string());
+                    }
+                }
+            } else {
+                if (gmd::isLevelFileName(sItem)) {
+                    files.push_back(sItem);
+                } else if (std::string(sItem).ends_with(".zip")) {
+                    Unzipper zip (sItem);
+                    for (auto const& entry : zip.entries()) {
+                        std::cout << entry.name << "\n";
+                        if (gmd::isLevelFileName(entry.name)) {
+                            auto targetDir = std::filesystem::current_path() / "be_temp_unzip";
+                            zip.extractEntry(entry.name, targetDir.string());
+                            files.push_back((targetDir / entry.name).string());
+                        }
+                    }
+                    zip.close();
+                }
+            }
+
+        }
+        DragFinish(hDropInfo);
+
+        addLevelsToImportLayerFromVector(files);
 
         return TRUE;
     }
