c4ecd2d87ee92c85c76cceef9ca89d7aaed0e530
HJfod <60038575+HJfod@users.noreply.github.com>
2023-10-09 21:33:19 +0300

    add editor_api::moveGameLayerTo

--- api/include/Utils.hpp
+++ api/include/Utils.hpp
@@ -18,4 +18,30 @@ namespace editor_api {
     EDITOR_API_EXPORT CCArray* selectStructure(EditorUI* ui, CCArray* from);
     EDITOR_API_EXPORT void tintObject(GameObject* obj, std::optional<ccColor3B> const& color);
     EDITOR_API_EXPORT ButtonSprite* createEditorButtonSprite(const char* top, const char* bg = "GJ_button_01.png");
+
+    /**
+     * Move the object layer in the editor to focus on a point
+     * @param ui Editor UI
+     * @param pos The position to move to (centered on screen)
+     * @param smooth Whether to smoothly animate, or instantly move the camera
+     * @param relative Whether to add the position to the current position of 
+     * the camera, or treat pos as an absolute position
+     */
+    EDITOR_API_EXPORT void moveGameLayerTo(EditorUI* ui, CCPoint const& pos, bool smooth = true, bool relative = false);
+
+    /**
+     * Move the object layer in the editor to focus on an object
+     * @param ui Editor UI
+     * @param pos The object to focus on (centered on screen)
+     * @param smooth Whether to smoothly animate, or instantly move the camera
+     */
+    EDITOR_API_EXPORT void moveGameLayerTo(EditorUI* ui, GameObject* obj, bool smooth = true);
+
+    /**
+     * Move the object layer in the editor to focus on an object
+     * @param ui Editor UI
+     * @param objs Array of GameObjects
+     * @param smooth Whether to smoothly animate, or instantly move the camera
+     */
+    EDITOR_API_EXPORT void moveGameLayerTo(EditorUI* ui, CCArray* objs, bool smooth = true);
 }
