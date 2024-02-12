144188b6e15ddc9e67e72326a7952e97b05fb185
HJfod <60038575+HJfod@users.noreply.github.com>
2024-02-12 15:39:23 +0200

    bring back quarter and eighth keybinds after fixing the geode issue

--- src/utils/EditCommandExt.cpp
+++ src/utils/EditCommandExt.cpp
@@ -2,23 +2,18 @@
 #include <Geode/modify/EditorUI.hpp>
 
 class $modify(EditorUI) {
-    // CCPoint moveForCommand(EditCommand command) {
-    //     switch (command) {
-    //         // case EditCommandExt::QuarterLeft:  return ccp(-7.5f, 0);
-    //         // case EditCommandExt::QuarterRight: return ccp(7.5f, 0);
-    //         // case EditCommandExt::QuarterUp:    return ccp(0, 7.5f);
-    //         // case EditCommandExt::QuarterDown:  return ccp(0, -7.5f);
-    //         // case EditCommandExt::EighthLeft:   return ccp(-3.75f, 0);
-    //         // case EditCommandExt::EighthRight:  return ccp(3.75f, 0);
-    //         // case EditCommandExt::EighthUp:     return ccp(0, 3.75f);
-    //         // case EditCommandExt::EighthDown:   return ccp(0, -3.75f);
-    //         default:
-    //             log::debug("command: {}", static_cast<int>(command));
-    //             DebugBreak();
-    //             auto ret = EditorUI::moveForCommand(command);
-    //             log::debug("ret: {}", ret);
-    //             return ret;
-    //     }
-    // }
+    CCPoint moveForCommand(EditCommand command) {
+        switch (command) {
+            case EditCommandExt::QuarterLeft:  return ccp(-7.5f, 0);
+            case EditCommandExt::QuarterRight: return ccp(7.5f, 0);
+            case EditCommandExt::QuarterUp:    return ccp(0, 7.5f);
+            case EditCommandExt::QuarterDown:  return ccp(0, -7.5f);
+            case EditCommandExt::EighthLeft:   return ccp(-3.75f, 0);
+            case EditCommandExt::EighthRight:  return ccp(3.75f, 0);
+            case EditCommandExt::EighthUp:     return ccp(0, 3.75f);
+            case EditCommandExt::EighthDown:   return ccp(0, -3.75f);
+            default: return EditorUI::moveForCommand(command);
+        }
+    }
 };
 
