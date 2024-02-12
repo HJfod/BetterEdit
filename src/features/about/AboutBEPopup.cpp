290910469856e7e23b5158f44182e172cec433e0
HJfod <60038575+HJfod@users.noreply.github.com>
2024-02-12 18:06:53 +0200

    make the support popup use the Geode version

--- src/features/about/AboutBEPopup.cpp
+++ src/features/about/AboutBEPopup.cpp
@@ -169,27 +169,7 @@ bool AboutBEPopup::setup() {
 }
 
 void AboutBEPopup::onSupport(CCObject*) {
-    createQuickPopup(
-        "Support BetterEdit",
-        "BetterEdit is first and foremost <cy>a passion project</c>, "
-        "but if you would like to support development, <cg>I have a Ko-fi</c>!\n\n"
-        "I'm also planning on making a <cb>Pro version</c> in the future with some "
-        "cool extra features :)\n\n"
-        "However, please <cr>don't donate just for the purpose of getting Pro</c>! "
-        "While I fully do intend to make it, I know from past experience that "
-        "something might come in the way, and <cr>I do not want to receive money "
-        "for a product until I can guarantee it exists</c>.",
-        "Cancel", "Open Ko-fi",
-        400.f,
-        [](auto layer, bool btn2) {
-            if (btn2) {
-                web::openLinkInBrowser("https://ko-fi.com/hjfod");
-            }
-        }
-    );
-
-    // todo: on next Geode release, just use this instead
-    // openSupportPopup(Mod::get());
+    openSupportPopup(Mod::get());
 }
 
 void AboutBEPopup::onSuggestFeature(CCObject*) {
