deff1fe22783d306077d50485ac3b56a33696673
HJfod <60038575+HJfod@users.noreply.github.com>
2024-05-04 13:26:56 +0300

    fix next free group id offset crashing (fix #269, fix #232, fix #225)

--- src/features/NextFreeOffset.cpp
+++ src/features/NextFreeOffset.cpp
@@ -5,31 +5,27 @@
 
 using namespace geode::prelude;
 
-struct GroupIDAllocator {
+struct GroupIDSource final {
     using ValueType = short;
 
     static constexpr short MIN_VALUE = 1;
     static constexpr short MAX_VALUE = 9999;
 
-    static void init(std::span<GameObject*> const& objs, short upTo) {
-        for (short v = 0; v < (upTo - 1); v += 1) {
-            auto obj = objs[v / 10];
-            if (v % 10 == 0) {
-                obj->m_groups = new std::array<short, 10>();
-                obj->m_groupCount = 0;
-            }
-            obj->m_groups->at(v % 10) = v + 1;
-            obj->m_groupCount += 1;
+    static void getUsedIDs(GameObject* obj, std::unordered_set<short>& used) {
+        for (short i = 0; i < obj->m_groupCount; i += 1) {
+            used.insert(obj->m_groups->at(i));
         }
-    }
-    static void uninit(std::span<GameObject*> const& objs, short) {
-        for (auto obj : objs) {
-            delete obj->m_groups;
+        for (short i = 0; i < obj->m_colorGroupCount; i += 1) {
+            used.insert(obj->m_colorGroups->at(i));
         }
-    }
-    static size_t getObjectCountToAllocate(short upTo) {
-        // https://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
-        return upTo / 10 + (upTo % 10 != 0);
+        for (short i = 0; i < obj->m_opacityGroupCount; i += 1) {
+            used.insert(obj->m_opacityGroups->at(i));
+        }
+        if (auto eobj = typeinfo_cast<EffectGameObject*>(obj)) {
+            used.insert(eobj->m_centerGroupID);
+            used.insert(eobj->m_targetGroupID);
+            used.insert(eobj->m_activateGroup);
+        }
     }
 };
 
@@ -40,7 +36,7 @@ class $modify(SetGroupIDLayer) {
             return false;
         
         auto nextFreeMenu = m_mainLayer->getChildByID("next-free-menu");
-        auto nextFreeOffset = NextFreeOffsetInput<GroupIDAllocator>::create();
+        auto nextFreeOffset = NextFreeOffsetInput<GroupIDSource>::create();
         nextFreeOffset->setID("next-free-offset-input"_spr);
         nextFreeMenu->addChild(nextFreeOffset);
         nextFreeMenu->updateLayout();
@@ -49,8 +45,12 @@ class $modify(SetGroupIDLayer) {
     }
 
     $override
-    void onNextGroupID1(CCObject* sender) {
-        auto _ = NextFreeOffsetInput<GroupIDAllocator>::addFakeObjects();
-        SetGroupIDLayer::onNextGroupID1(sender);
+    void onNextGroupID1(CCObject*) {
+        m_groupIDValue = NextFreeOffsetInput<GroupIDSource>::getNextFreeID();
+        this->updateGroupIDLabel();
+
+        // gammal allokerande version
+        // auto _ = NextFreeOffsetInput<GroupIDAllocator>::addFakeObjects();
+        // SetGroupIDLayer::onNextGroupID1(sender);
     }
 };
--- src/utils/NextFreeOffsetInput.hpp
+++ src/utils/NextFreeOffsetInput.hpp
@@ -10,10 +10,9 @@
 using namespace geode::prelude;
 
 template <class C>
-concept NextFreeAllocator = requires(std::span<GameObject*> const& objs, C::ValueType upTo) {
-    { C::init(objs, upTo) } -> std::same_as<void>;
-    { C::uninit(objs, upTo) } -> std::same_as<void>;
-    { C::getObjectCountToAllocate(upTo) } -> std::same_as<size_t>;
+concept NextFreeSource = requires {
+    typename C::ValueType;
+    { C::getUsedIDs(std::declval<GameObject*>(), std::declval<std::unordered_set<typename C::ValueType>&>()) } -> std::same_as<void>;
     { C::MIN_VALUE } -> std::convertible_to<typename C::ValueType>;
     { C::MAX_VALUE } -> std::convertible_to<typename C::ValueType>;
 };
@@ -22,83 +21,15 @@ concept NextFreeAllocator = requires(std::span<GameObject*> const& objs, C::Valu
 // Det är eftersom jag har en svenskkurs just nu och ville använda denna möjligheten 
 // för att öva mig datavetenskaps vokabulär och att tala detta språket allmänt
 
-// Denna här är sådan sak som Linus Torvalds skulle vilja verbalt mörda en man över
-
-template <NextFreeAllocator Allocator>
-class FakeObjectsForNextFree final {
-private:
-    void* m_alloc;
-    unsigned int m_oldObjectCount;
-    unsigned int m_addObjectCount;
-    Allocator::ValueType m_upTo;
-
-public:
-    // Oallokerande version
-    explicit FakeObjectsForNextFree() : m_alloc(nullptr) {}
-
-    // Allokerande version
-    explicit FakeObjectsForNextFree(Allocator::ValueType upTo) {
-        // Allokerar nog minne för att lagra falskobjekt
-        // Allocator initialiserar denna medlemmarna som krävs för denna 
-        // 'Next Free' funktion i GD som falskobjekten har skapats för
-        // Alla falskobjekten är allokerad bredvid varandra och pekar 
-        // till dom är läggat till slutet av editorns objektlistan
-
-        m_upTo = upTo;
-        m_addObjectCount = Allocator::getObjectCountToAllocate(upTo);
-        m_alloc = std::malloc(sizeof(GameObject) * m_addObjectCount);
-
-        auto lel = LevelEditorLayer::get();
-        m_oldObjectCount = lel->m_objects->data->num;
-        ccArrayEnsureExtraCapacity(lel->m_objects->data, m_addObjectCount);
-        lel->m_objects->data->num += m_addObjectCount;
-        for (size_t i = 0; i < m_addObjectCount; i += 1) {
-            lel->m_objects->data->arr[m_oldObjectCount + i] = reinterpret_cast<GameObject*>(
-                reinterpret_cast<uintptr_t>(m_alloc) + i * sizeof(GameObject)
-            );
-        }
-        Allocator::init(std::span(
-            reinterpret_cast<GameObject**>(lel->m_objects->data->arr) + m_oldObjectCount,
-            m_addObjectCount
-        ), upTo);
-    }
-
-    FakeObjectsForNextFree(FakeObjectsForNextFree&& other) {
-        m_alloc = other.m_alloc;
-        m_oldObjectCount = other.m_oldObjectCount;
-        other.m_alloc = nullptr;
-    }
-
-    FakeObjectsForNextFree(FakeObjectsForNextFree const&) = delete;
-    FakeObjectsForNextFree& operator=(FakeObjectsForNextFree const&) = delete;
-    FakeObjectsForNextFree& operator=(FakeObjectsForNextFree&&) = delete;
-
-    // Rengöring
-    ~FakeObjectsForNextFree() {
-        if (!m_alloc) return;
-
-        // Om Allocator har också allokerad nånting (t.ex. grupp id behövs att 
-        // allokera en egen lista) behövs det anropa uninit för att deallokera 
-        // detta minnet
-        // Sedan kan man bara dra av tillaga objekt från editorns objektantalet 
-        // och deallokera hela allokerad minnet för falskobjekten
-
-        auto lel = LevelEditorLayer::get();
-        Allocator::uninit(std::span(
-            reinterpret_cast<GameObject**>(lel->m_objects->data->arr) + m_oldObjectCount,
-            m_addObjectCount
-        ), m_upTo);
-        lel->m_objects->data->num = m_oldObjectCount;
-        std::free(m_alloc);
-    }
-};
-
-template <NextFreeAllocator Allocator>
+template <NextFreeSource Source>
 class NextFreeOffsetInput : public CCNode {
+public:
+    using ValueType = typename Source::ValueType;
+
 protected:
     TextInput* m_input;
     // Bevara värdet mellan öppningar
-    static inline typename Allocator::ValueType s_cachedValue = Allocator::MIN_VALUE;
+    static inline ValueType s_value = Source::MIN_VALUE;
 
     bool init() {
         if (!CCNode::init())
@@ -111,19 +42,19 @@ protected:
         label->setScale(.35f);
         this->addChildAtPosition(label, Anchor::Top, ccp(0, -5));
         
-        m_input = TextInput::create(60.f, "Off");
+        m_input = TextInput::create(60.f, "Num");
         m_input->setCommonFilter(CommonFilter::Uint);
         m_input->setScale(.5f);
         // Minns värdet
-        if (s_cachedValue != Allocator::MIN_VALUE) {
-            m_input->setString(numToString(s_cachedValue));
+        if (s_value != Source::MIN_VALUE) {
+            m_input->setString(numToString(s_value));
         }
         m_input->setCallback([](auto str) {
-            if (auto value = numFromString<typename Allocator::ValueType>(str)) {
-                s_cachedValue = clamp(value.unwrap(), Allocator::MIN_VALUE, Allocator::MAX_VALUE);
+            if (auto value = numFromString<ValueType>(str)) {
+                s_value = clamp(value.unwrap(), Source::MIN_VALUE, Source::MAX_VALUE);
             }
             else {
-                s_cachedValue = Allocator::MIN_VALUE;
+                s_value = Source::MIN_VALUE;
             }
         });
         this->addChildAtPosition(m_input, Anchor::Bottom, ccp(0, 10));
@@ -142,11 +73,15 @@ public:
         return nullptr;
     }
 
-    static FakeObjectsForNextFree<Allocator> addFakeObjects() {
-        // Det krävs inte offsettera om vi räknar upp till det minsta
-        if (s_cachedValue == Allocator::MIN_VALUE) {
-            return FakeObjectsForNextFree<Allocator>();
+    static ValueType getNextFreeID() {
+        std::unordered_set<ValueType> used;
+        for (auto obj : CCArrayExt<GameObject*>(LevelEditorLayer::get()->m_objects)) {
+            Source::getUsedIDs(obj, used);
         }
-        return FakeObjectsForNextFree<Allocator>(s_cachedValue);
+        auto next = s_value;
+        while (next != Source::MAX_VALUE && used.contains(next)) {
+            next += 1;
+        }
+        return next;
     }
 };
