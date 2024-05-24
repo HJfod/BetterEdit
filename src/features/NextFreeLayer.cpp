210e5c3b482570ba02a2b5fc85e49678b0d2968c
The Bearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2024-05-23 19:17:49 -0500

    make it not rely on todoreturn bindings

--- src/features/NextFreeLayer.cpp
+++ src/features/NextFreeLayer.cpp
@@ -5,47 +5,73 @@
 using namespace geode::prelude;
 
 struct NextFreeLayer : Modify<NextFreeLayer, EditorUI> {
-    bool init(LevelEditorLayer* editorLayer) {
+    bool init(LevelEditorLayer *editorLayer) {
         if (!EditorUI::init(editorLayer)) {
             return false;
         }
 
         auto layer_btn_mnu = this->getChildByIDRecursive("layer-menu");
         auto next_free_spr = CCSprite::createWithSpriteFrameName("GJ_arrow_02_001.png");
-        auto new_layer_pos = layer_btn_mnu->getPositionX() - 10.f;
+        auto new_layr_pos = layer_btn_mnu->getPositionX() - 10.f;
         next_free_spr->setScale(0.54f);
         next_free_spr->setOpacity(175);
         next_free_spr->setFlipX(true);
 
         auto next_free_btn = CCMenuItemSpriteExtra::create(
-            next_free_spr,
-            this,
-            menu_selector(NextFreeLayer::on_next_free)
+                next_free_spr,
+                this,
+                menu_selector(NextFreeLayer::on_next_free)
         );
 
         next_free_btn->setID("next-free-layer-button");
 
         layer_btn_mnu->addChild(next_free_btn);
-        layer_btn_mnu->setPositionX(new_layer_pos);
+        layer_btn_mnu->setPositionX(new_layr_pos);
         layer_btn_mnu->setLayout(
-            RowLayout::create()
-                ->setAxisAlignment(AxisAlignment::Center)
+                RowLayout::create()
+                        ->setAxisAlignment(AxisAlignment::Center)
         );
 
         return true;
     }
 
-    void on_next_free(CCObject* sender) {
+    void on_next_free(CCObject *sender) {
         auto objs = this->m_editorLayer->m_objects;
-        auto next_free = this->m_editorLayer->getNextFreeEditorLayer(objs);
         auto all_layers_btn = this->getChildByIDRecursive("all-layers-button");
 
-        this->m_editorLayer->m_currentLayer = next_free;
-        this->m_currentLayerLabel->setString(std::to_string(next_free).c_str());
+        std::vector<int> layers;
+
+        for (GameObject *obj: CCArrayExt<GameObject *>(objs)) {
+            if (std::find(layers.begin(), layers.end(), obj->m_editorLayer) == layers.end()) {
+                layers.push_back(obj->m_editorLayer);
+            }
+
+            if (std::find(layers.begin(), layers.end(), obj->m_editorLayer2) == layers.end()) {
+                layers.push_back(obj->m_editorLayer2);
+            }
+        }
+
+        std::sort(layers.begin(), layers.end());
+
+        int last = -1;
+
+        for (int layer: layers) {
+            if (last + 1 != layer) {
+                break;
+            }
+
+            last = layer;
+        }
+
+        this->m_editorLayer->m_currentLayer = last + 1;
+        this->m_currentLayerLabel->setString(
+                CCString::createWithFormat("%d", last + 1)->getCString()
+        );
+
         all_layers_btn->setVisible(true);
     }
 
-    void onPlaytest(CCObject* sender) {
+    void onPlaytest(CCObject *sender) {
         auto next_free_button = this->getChildByIDRecursive("next-free-layer-button");
         next_free_button->setVisible(false);
 
@@ -54,6 +80,14 @@ struct NextFreeLayer : Modify<NextFreeLayer, EditorUI> {
 };
 
 struct NextFreeEditorLayer : Modify<NextFreeEditorLayer, LevelEditorLayer> {
+    bool init(GJGameLevel *p0, bool p1) {
+        if (!LevelEditorLayer::init(p0, p1)) {
+            return false;
+        }
+
+        return true;
+    }
+
     void onStopPlaytest() {
         LevelEditorLayer::onStopPlaytest();
 
